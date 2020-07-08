618fecba34fb5b009215dd68b8675a52b615bb15
mindshire <harsha@mindshire.pro>
2020-07-08 22:37:06 +0530

    project setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());  
+
+
+app.listen(process.env.port, () =>
+ console.log(`Server started at port : ${process.env.port}`));
