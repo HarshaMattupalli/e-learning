d64410c89f18a562a20e4262267b4b9b9a6763f4
mindshire <harsha@mindshire.pro>
2020-07-14 10:22:43 +0530

    origin maste

--- server/models/db.js
+++ server/models/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://harsha:harsha123@cluster0.zowkh.mongodb.net/learning?retryWrites=true&w=majority";
+require('dotenv').config();
 
-
-var connection = mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true },
+var connection = mongoose.connect(process.env.MONGOURL, { useUnifiedTopology: true, useNewUrlParser: true },
     (err) => {
         if (!err) {
             console.log('db connected');
