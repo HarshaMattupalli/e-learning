89b8b614ab92edd678a0b6b786abeab72a41ca1d
mindshire <harsha@mindshire.pro>
2020-07-15 08:46:52 +0530

    master

--- server/routes/forgetPasswordMail.js
+++ server/routes/forgetPasswordMail.js
@@ -7,13 +7,13 @@ const nodemailer = require('nodemailer');
 router.get('/', (req, res) => {
 
   const oauth2Client = new OAuth2(
-    "380500289033-hj2g6u6osr9ueo9493mcm8is5raimhln.apps.googleusercontent.com", // ClientID
-    "QQ2qHtxvZ8yYM1J54QY5hUlq", // Client Secret
+    process.env.CLIENT_ID, // ClientID
+    process.env.CLINET_SECRETE, // Client Secret
     "https://developers.google.com/oauthplayground" // Redirect URL
   );
 
   oauth2Client.setCredentials({
-    refresh_token: "1//04F8pBNK-ktWHCgYIARAAGAQSNwF-L9Ir-qHCGMZ9knndb2Tsxt7ZD5gsr98jGJ3ydlAnG_fS4O3tC2U1crnq5PzdHPTavPCPrYQ"
+    refresh_token: process.env.REFRESH_TOKEN
   });
   const accessToken = oauth2Client.getAccessToken()
   
@@ -22,10 +22,10 @@ router.get('/', (req, res) => {
     auth: {
       type: "OAuth2",
       user: "harsha@mindshire.pro",
-      clientId: "380500289033-hj2g6u6osr9ueo9493mcm8is5raimhln.apps.googleusercontent.com",
-      clientSecret: "QQ2qHtxvZ8yYM1J54QY5hUlq",
-      refreshToken: "1//04F8pBNK-ktWHCgYIARAAGAQSNwF-L9Ir-qHCGMZ9knndb2Tsxt7ZD5gsr98jGJ3ydlAnG_fS4O3tC2U1crnq5PzdHPTavPCPrYQ",
-      accessToken: "ya29.a0AfH6SMD7YOxkW4tReo3arSwL7rQQmTQWa6WMMgaV8hCqJqjMoP_TM8TS-cDiocOtJQb1YKtLbRiwxCmdiY4iCWU-Z6ulTdAirH83_HYBgPDW_fDYPEdDZu7OHecmsxFsEx4GJlmAjpqIV5Fa0PZ57_waJ6muYptpU54"
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLINET_SECRETE,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN
     }
   });
   const mailOptions = {
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
     to: "harsha@mindshire.pro",
     subject: "Node.js Email with Secure OAuth",
     generateTextFromHTML: true,
-    html: "<b>test</b>"
+    html: "<b>changed</b>"
   };
   smtpTransport.sendMail(mailOptions, (error, response) => {
     error ? console.log(error) : console.log(response);
